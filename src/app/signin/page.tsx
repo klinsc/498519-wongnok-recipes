c38662c927eb975dca0cd3dc465745822ad286f3
Chatbordin <klinscea@gmail.com>
2025-05-16 18:31:20 +0700

    initial

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,26 +1,26 @@
-import * as React from 'react'
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
+import MuiCard from '@mui/material/Card'
 import Checkbox from '@mui/material/Checkbox'
 import CssBaseline from '@mui/material/CssBaseline'
-import FormControlLabel from '@mui/material/FormControlLabel'
 import Divider from '@mui/material/Divider'
-import FormLabel from '@mui/material/FormLabel'
 import FormControl from '@mui/material/FormControl'
+import FormControlLabel from '@mui/material/FormControlLabel'
+import FormLabel from '@mui/material/FormLabel'
 import Link from '@mui/material/Link'
+import Stack from '@mui/material/Stack'
+import { styled } from '@mui/material/styles'
 import TextField from '@mui/material/TextField'
 import Typography from '@mui/material/Typography'
-import Stack from '@mui/material/Stack'
-import MuiCard from '@mui/material/Card'
-import { styled } from '@mui/material/styles'
-import ForgotPassword from '~/app/_components/Singin/ForgotPassword'
+import * as React from 'react'
 import AppTheme from '~/app/_components/shared-theme/AppTheme'
 import ColorModeSelect from '~/app/_components/shared-theme/ColorModeSelect'
 import {
-  GoogleIcon,
   FacebookIcon,
+  GoogleIcon,
   SitemarkIcon,
-} from '~/app/_components/Singin/CustomIcons'
+} from '~/app/_components/Signin/CustomIcons'
+import ForgotPassword from '~/app/_components/Signin/ForgotPassword'
 
 const Card = styled(MuiCard)(({ theme }) => ({
   display: 'flex',
