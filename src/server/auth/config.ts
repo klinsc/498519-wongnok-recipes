7314bb91f172ef26f9397ce29ec3a3a8f5d6f92c
Chatbordin <klinscea@gmail.com>
2025-05-16 21:11:58 +0700

    added: signing in algorithm

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -1,8 +1,7 @@
-import { PrismaAdapter } from '@auth/prisma-adapter'
 import { type DefaultSession, type NextAuthConfig } from 'next-auth'
-import DiscordProvider from 'next-auth/providers/discord'
-
-import { db } from '~/server/db'
+import CredentialsProvider from 'next-auth/providers/credentials'
+import { env } from '~/env'
+import { db } from '../db'
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
@@ -32,18 +31,42 @@ declare module 'next-auth' {
  */
 export const authConfig = {
   providers: [
-    DiscordProvider,
-    /**
-     * ...add more providers here.
-     *
-     * Most other providers require a bit more work than the Discord provider. For example, the
-     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-     *
-     * @see https://next-auth.js.org/providers/github
-     */
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: { label: 'Email', type: 'text' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials) {
+        const { email, password } = credentials as {
+          email: string
+          password: string
+        }
+
+        // Query the database for the user
+        const user = await db.user.findFirst({
+          where: {
+            email,
+            password,
+          },
+        })
+        if (!user) {
+          throw new Error('No user found with the email')
+        }
+
+        return null
+      },
+    }),
   ],
-  adapter: PrismaAdapter(db),
+  // Uncomment the following line to use a database adapter
+  // adapter: PrismaAdapter(db),
+
+  secret: env.AUTH_SECRET,
+
+  session: {
+    strategy: 'jwt', // Use JWT-based sessions
+  },
+
   callbacks: {
     session: ({ session, user }) => ({
       ...session,
