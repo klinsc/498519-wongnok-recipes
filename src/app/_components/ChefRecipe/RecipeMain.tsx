641cdc48e749b7e45c0bd9500c75bddada1a8d6a
Chatbordin <klinscea@gmail.com>
2025-05-17 21:24:42 +0700

    feat: refactor RecipeMain by extracting RecipeMainActions component for improved code organization

--- src/app/_components/ChefRecipe/RecipeMain.tsx
+++ src/app/_components/ChefRecipe/RecipeMain.tsx
@@ -1,16 +1,11 @@
 /* eslint-disable @typescript-eslint/prefer-nullish-coalescing */
-import CloseIcon from '@mui/icons-material/Close'
-import FavoriteIcon from '@mui/icons-material/Favorite'
-import MoreVertIcon from '@mui/icons-material/MoreVert'
-import SaveIcon from '@mui/icons-material/Save'
-import ShareIcon from '@mui/icons-material/Share'
-import { Box, Menu, MenuItem, Stack, TextField } from '@mui/material'
+
+import { Box, TextField } from '@mui/material'
 import Avatar from '@mui/material/Avatar'
 import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
 import CardHeader from '@mui/material/CardHeader'
 import CardMedia from '@mui/material/CardMedia'
-import IconButton from '@mui/material/IconButton'
 import Typography from '@mui/material/Typography'
 import type { RecipeDetail, RecipeName, User } from '@prisma/client'
 import dayjs from 'dayjs'
@@ -27,16 +22,17 @@ import {
   useEffect,
   useMemo,
   useState,
-  type MouseEvent,
 } from 'react'
 import { api } from '~/trpc/react'
 import { stringAvatar } from '../AppAvatar'
 import ImageUploader from '../ImageUploader'
+import RecipeMainActions from './RecipeMainActions'
 
 dayjs.extend(utc)
 dayjs.extend(timezone)
 
-interface RecipeNameWithCreatedByAndDetail extends RecipeName {
+export interface RecipeNameWithCreatedByAndDetail
+  extends RecipeName {
   createdBy: User
   RecipeDetail: RecipeDetail
 }
@@ -44,112 +40,6 @@ interface RecipeNameWithCreatedByAndDetail extends RecipeName {
 interface RecipeMainProps {
   userID: string
   recipeID: string
-}
-
-interface RecipeMainActionsProps {
-  handleDeleteRecipeDraft: () => void
-  handleSave: () => void
-  handleCancel: () => void
-  isDeleteRecipeDraftPending: boolean
-  isEditting: {
-    name: boolean
-    detail: boolean
-  }
-  currentRecipe: RecipeNameWithCreatedByAndDetail | null
-  userID: string
-}
-
-function RecipeMainActions({
-  handleDeleteRecipeDraft,
-  handleSave,
-  handleCancel,
-  isDeleteRecipeDraftPending,
-  isEditting,
-  currentRecipe,
-  userID,
-}: RecipeMainActionsProps) {
-  // navigation: Router
-  const router = useRouter()
-  // navigation: Path name
-  const pathName = usePathname()
-
-  // State: Menu
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
-  const open = Boolean(anchorEl)
-  const handleClick = useCallback(
-    (event: MouseEvent<HTMLButtonElement>) => {
-      setAnchorEl(event.currentTarget)
-    },
-    [],
-  )
-  const handleClose = useCallback(() => {
-    setAnchorEl(null)
-  }, [])
-
-  // Callback: handleEdit
-  const handleEdit = useCallback(() => {
-    void router.push(`${pathName}?editing=true`, {
-      scroll: false,
-    })
-
-    handleClose()
-  }, [handleClose, pathName, router])
-
-  return (
-    <Stack direction="row" spacing={1}>
-      {isEditting.name ? (
-        <>
-          <IconButton
-            aria-label="save recipe name"
-            onClick={handleSave}>
-            <SaveIcon />
-          </IconButton>
-          <IconButton
-            aria-label="close recipe name"
-            onClick={handleCancel}>
-            <CloseIcon />
-          </IconButton>
-        </>
-      ) : (
-        <>
-          <IconButton aria-label="add to favorites">
-            <FavoriteIcon />
-          </IconButton>
-          <IconButton disabled aria-label="share">
-            <ShareIcon />
-          </IconButton>
-          <IconButton
-            disabled={
-              currentRecipe?.createdById !== userID ||
-              isDeleteRecipeDraftPending
-            }
-            id="draft-settings-button"
-            aria-controls={open ? 'draft-settings-menu' : undefined}
-            aria-haspopup="true"
-            aria-expanded={open ? 'true' : undefined}
-            onClick={handleClick}
-            sx={{ color: '#000' }}>
-            <MoreVertIcon />
-          </IconButton>
-          <Menu
-            id="draft-settings-menu"
-            anchorEl={anchorEl}
-            open={open}
-            onClose={handleClose}
-            MenuListProps={{
-              'aria-labelledby': 'draft-settings-button',
-            }}>
-            <MenuItem onClick={handleEdit}>แก้ไขสูตร</MenuItem>
-            <MenuItem
-              disabled={isDeleteRecipeDraftPending}
-              onClick={handleDeleteRecipeDraft}>
-              ลบ
-            </MenuItem>
-          </Menu>
-        </>
-      )}
-    </Stack>
-  )
 }
 
 export default memo(function RecipeMain(props: RecipeMainProps) {
---
+++ src/app/_components/ChefRecipe/RecipeMainActions.tsx
@@ -0,0 +1,115 @@
+import { usePathname, useRouter } from 'next/navigation'
+import { memo, useCallback, useState, type MouseEvent } from 'react'
+import type { RecipeNameWithCreatedByAndDetail } from './RecipeMain'
+import { IconButton, Menu, MenuItem, Stack } from '@mui/material'
+import CloseIcon from '@mui/icons-material/Close'
+import SaveIcon from '@mui/icons-material/Save'
+import FavoriteIcon from '@mui/icons-material/Favorite'
+import MoreVertIcon from '@mui/icons-material/MoreVert'
+import ShareIcon from '@mui/icons-material/Share'
+
+interface RecipeMainActionsProps {
+  handleDeleteRecipeDraft: () => void
+  handleSave: () => void
+  handleCancel: () => void
+  isDeleteRecipeDraftPending: boolean
+  isEditting: {
+    name: boolean
+    detail: boolean
+  }
+  currentRecipe: RecipeNameWithCreatedByAndDetail | null
+  userID: string
+}
+
+export default memo(function RecipeMainActions({
+  handleDeleteRecipeDraft,
+  handleSave,
+  handleCancel,
+  isDeleteRecipeDraftPending,
+  isEditting,
+  currentRecipe,
+  userID,
+}: RecipeMainActionsProps) {
+  // navigation: Router
+  const router = useRouter()
+  // navigation: Path name
+  const pathName = usePathname()
+
+  // State: Menu
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+  const open = Boolean(anchorEl)
+  const handleClick = useCallback(
+    (event: MouseEvent<HTMLButtonElement>) => {
+      setAnchorEl(event.currentTarget)
+    },
+    [],
+  )
+  const handleClose = useCallback(() => {
+    setAnchorEl(null)
+  }, [])
+
+  // Callback: handleEdit
+  const handleEdit = useCallback(() => {
+    void router.push(`${pathName}?editing=true`, {
+      scroll: false,
+    })
+
+    handleClose()
+  }, [handleClose, pathName, router])
+
+  return (
+    <Stack direction="row" spacing={1}>
+      {isEditting.name ? (
+        <>
+          <IconButton
+            aria-label="save recipe name"
+            onClick={handleSave}>
+            <SaveIcon />
+          </IconButton>
+          <IconButton
+            aria-label="close recipe name"
+            onClick={handleCancel}>
+            <CloseIcon />
+          </IconButton>
+        </>
+      ) : (
+        <>
+          <IconButton aria-label="add to favorites">
+            <FavoriteIcon />
+          </IconButton>
+          <IconButton disabled aria-label="share">
+            <ShareIcon />
+          </IconButton>
+          <IconButton
+            disabled={
+              currentRecipe?.createdById !== userID ||
+              isDeleteRecipeDraftPending
+            }
+            id="draft-settings-button"
+            aria-controls={open ? 'draft-settings-menu' : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? 'true' : undefined}
+            onClick={handleClick}
+            sx={{ color: '#000' }}>
+            <MoreVertIcon />
+          </IconButton>
+          <Menu
+            id="draft-settings-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+              'aria-labelledby': 'draft-settings-button',
+            }}>
+            <MenuItem onClick={handleEdit}>แก้ไขสูตร</MenuItem>
+            <MenuItem
+              disabled={isDeleteRecipeDraftPending}
+              onClick={handleDeleteRecipeDraft}>
+              ลบ
+            </MenuItem>
+          </Menu>
+        </>
+      )}
+    </Stack>
+  )
+})
