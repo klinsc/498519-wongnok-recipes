17a3e5d322baf644425abb2d3c46189c7cf05964
Chatbordin <klinscea@gmail.com>
2025-05-17 13:07:33 +0700

    feat: enhance MyDraftRecipe component with settings menu for user actions

--- src/app/_components/ChefRecipe/MyDraftRecipe.tsx
+++ src/app/_components/ChefRecipe/MyDraftRecipe.tsx
@@ -1,24 +1,58 @@
 import MoreVertIcon from '@mui/icons-material/MoreVert'
+import { Menu, MenuItem } from '@mui/material'
 import Card from '@mui/material/Card'
 import CardHeader from '@mui/material/CardHeader'
 import IconButton from '@mui/material/IconButton'
 import type { RecipeName } from '@prisma/client'
 import dayjs from 'dayjs'
+import { useState, type MouseEvent } from 'react'
 
 export interface MyDraftRecipeProps {
   recipe: RecipeName
 }
 
 export default function MyDraftRecipe(props: MyDraftRecipeProps) {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+  const open = Boolean(anchorEl)
+  const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
   return (
     <>
       <Card sx={{ maxWidth: 345, backgroundColor: '#f5f5f5' }}>
         <CardHeader
           avatar={false}
           action={
-            <IconButton aria-label="settings">
-              <MoreVertIcon />
-            </IconButton>
+            <>
+              <IconButton
+                id="draft-settings-button"
+                aria-controls={
+                  open ? 'draft-settings-menu' : undefined
+                }
+                aria-haspopup="true"
+                aria-expanded={open ? 'true' : undefined}
+                onClick={handleClick}
+                sx={{ color: '#000' }}>
+                <MoreVertIcon />
+              </IconButton>
+
+              <Menu
+                id="draft-settings-menu"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                MenuListProps={{
+                  'aria-labelledby': 'draft-settings-button',
+                }}>
+                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={handleClose}>My account</MenuItem>
+                <MenuItem onClick={handleClose}>Logout</MenuItem>
+              </Menu>
+            </>
           }
           title={props.recipe.name || 'ชื่อสูตรอาหาร'}
           subheader={
