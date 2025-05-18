baf332231e45b40f6760eef67ea0da192cb11739
Chatbordin <klinscea@gmail.com>
2025-05-18 21:12:29 +0700

    feat: simplify Home component by removing session handling and unnecessary markup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,10 @@
-import { auth } from '~/server/auth'
-import { api, HydrateClient } from '~/trpc/server'
+import { HydrateClient } from '~/trpc/server'
 import Blog from './_components/Blog/Main'
 
 export default async function Home() {
-  const session = await auth()
-  console.log('session', session)
-
-  if (session?.user) {
-    void api.post.getLatest.prefetch()
-  }
-
   return (
     <HydrateClient>
-      <main>
-        <Blog />
-      </main>
+      <Blog />
     </HydrateClient>
   )
 }
