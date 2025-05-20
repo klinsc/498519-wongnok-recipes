116c0c50d8692687a533080b1ce19e64967c81e3
Chatbordin <klinscea@gmail.com>
2025-05-20 16:29:37 +0700

    refactor: enhance Breadcrumb component to fetch user and recipe names via TRPC

--- src/app/_components/Breadcrumb/index.tsx
+++ src/app/_components/Breadcrumb/index.tsx
@@ -1,38 +1,93 @@
+/* eslint-disable @typescript-eslint/prefer-nullish-coalescing */
 import Breadcrumbs from '@mui/material/Breadcrumbs'
 import Link from '@mui/material/Link'
-import { usePathname } from 'next/navigation'
-import { MENU_ITEMS } from '../Hamburger'
+import { useMemo } from 'react'
+import { api } from '~/trpc/react'
 
-export default function Breadcrumb() {
-  const pathname = usePathname()
+interface BreadcrumbProps {
+  userID?: string
+  recipeID?: string
+}
+
+export default function Breadcrumb(props: BreadcrumbProps) {
+  // Trpc: get user name by id
+  const { data: userName } = api.user.getNameById.useQuery(
+    {
+      id: props.userID || '',
+    },
+    {
+      enabled: !!props.userID,
+    },
+  )
+
+  // Trpc: get recipe name by id
+  const { data: recipeName } = api.recipe.getNameById.useQuery(
+    {
+      recipeId: props.recipeID || '',
+    },
+    {
+      enabled:
+        !!props.recipeID &&
+        props.recipeID !== 'all' &&
+        props.recipeID !== 'new',
+    },
+  )
+
+  // Memo: hightlight the current page
+  const hightlightIndex = useMemo(() => {
+    if (
+      props.userID &&
+      props.recipeID &&
+      props.recipeID !== 'all' &&
+      props.recipeID !== 'new'
+    ) {
+      return 2
+    }
+    if (props.userID && props.recipeID === 'all') {
+      return 1
+    }
+    return 0
+  }, [props.userID, props.recipeID])
 
   return (
-    <div>
-      <Breadcrumbs aria-label="breadcrumb">
+    <Breadcrumbs aria-label="breadcrumb">
+      <Link
+        href="/"
+        sx={{
+          color:
+            hightlightIndex === 0
+              ? 'text.primary'
+              : 'text.secondary',
+          fontWeight: hightlightIndex === 0 ? 'bold' : 'normal',
+        }}>
+        หน้าหลัก
+      </Link>
+      {userName && (
         <Link
+          href={`/chef/${props.userID}/recipe/all`}
           sx={{
             color:
-              pathname === '/' ? 'text.primary' : 'text.secondary',
-          }}
-          underline="hover"
-          href="/">
-          หน้าหลัก
+              hightlightIndex === 1
+                ? 'text.primary'
+                : 'text.secondary',
+            fontWeight: hightlightIndex === 1 ? 'bold' : 'normal',
+          }}>
+          {`เชฟ ${userName}`}
         </Link>
-        {MENU_ITEMS.filter(
-          (item) =>
-            item.path !== '/' && pathname?.includes(item.path),
-        ).map((item) => (
-          <Link
-            sx={{
-              color: 'text.primary',
-            }}
-            key={item.label}
-            underline="hover"
-            href={item.path}>
-            {item.label}
-          </Link>
-        ))}
-      </Breadcrumbs>
-    </div>
+      )}
+      {recipeName && (
+        <Link
+          href={`/chef/${props.userID}/recipe/${props.recipeID}`}
+          sx={{
+            color:
+              hightlightIndex === 2
+                ? 'text.primary'
+                : 'text.secondary',
+            fontWeight: hightlightIndex === 2 ? 'bold' : 'normal',
+          }}>
+          {`สูตร ${recipeName}`}
+        </Link>
+      )}
+    </Breadcrumbs>
   )
 }
