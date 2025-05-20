416cbc286e62e983d38e7cc3dec78f1087988dbd
Chatbordin <klinscea@gmail.com>
2025-05-20 17:37:34 +0700

    fix: update next.config.js to correct syntax and maintain react strict mode setting

--- next.config.js
+++ next.config.js
@@ -2,9 +2,12 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-import "./src/env.js";
+import './src/env.js'
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  // disable react strict mode
+  reactStrictMode: false,
+}
 
-export default config;
+export default config
